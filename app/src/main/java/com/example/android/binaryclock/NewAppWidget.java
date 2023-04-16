1328c809e7adc622a71debfba3e6d8c0f9f19d30
Vitaliy Emel'yanov <I-em-vitaliy@yandex.ru>
2023-04-16 21:05:54 +0300

    refactoring

--- app/src/main/java/com/example/android/binaryclock/NewAppWidget.java
+++ app/src/main/java/com/example/android/binaryclock/NewAppWidget.java
@@ -115,12 +115,6 @@ public class NewAppWidget extends AppWidgetProvider {
         PendingIntent pIntent = PendingIntent.getBroadcast(context, appWidgetId, updateIntent, 0);
         views.setOnClickPendingIntent(R.id.reFRESH, pIntent);
 
-
-
-
-
-
-        // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
 
 
@@ -129,34 +123,24 @@ public class NewAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-
-
-
-        // There may be multiple widgets active, so update all of them
-
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
-
         }
     }
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
     public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+
     }
     @Override
     public void onReceive(Context context, Intent intent) {
-
-
         super.onReceive(context, intent);
 
-
-//Do your task here like updating the textviews
     }
 
 }
